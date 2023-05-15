f1be74061ab0df7550c6732004550aa9282af97e
Abdulkadir Celebi <abdulkadircelebi.ac@gmail.com>
2023-05-15 18:37:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+class Cat {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+	speak() {
+		return `${this.name} says meow!`;
+	}
+}
+class Dog {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+	speak() {
+		return `${this.name} says woof!`;
+	}
+}
+class Bird {
+	constructor(name, sex) {
+		this.name = name;
+		this.sex = sex;
+	}
+	speak() {
+		if (this.sex === 'male') {
+			return `It's me! ${this.name}, the parrot!`;
+		} else {
+			return `${this.name} says squawk!`;
+		}
+	}
+}
+
+const bird = new Bird('Pablo', 'male');
+const dog = new Dog('Scooby');
+const cat = new Cat('Garfield');
+const bird2 = new Bird('Mable', 'female');
+console.log(bird.speak());
